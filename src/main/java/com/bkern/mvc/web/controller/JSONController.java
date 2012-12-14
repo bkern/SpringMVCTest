48cff6d0cf8a9b760b8782df421cb63ff77c8703
Barry Kern <bskern@gmail.com>
2012-12-14 07:01:46 -0600

    Adding new method to JSONController and tests around POSTs

--- src/main/java/com/bkern/mvc/web/controller/JSONController.java
+++ src/main/java/com/bkern/mvc/web/controller/JSONController.java
@@ -1,13 +1,17 @@
 package com.bkern.mvc.web.controller;
 
 import com.bkern.mvc.domain.UserData;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class JSONController {
 
-    @RequestMapping(value = {"/getData/{userId}"}, method = RequestMethod.GET, produces = "application/json")
+    @RequestMapping(value = {"/getData/{userId}"}, method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public UserData getData(@PathVariable("userId") String userId) throws Exception {
 
@@ -20,4 +24,24 @@ public class JSONController {
         return ud;
     }
 
+    @RequestMapping(value = {"/user/"}, method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public String saveUser(@RequestBody UserData userData,HttpServletResponse response) throws Exception {
+
+        if(userData.getAge() > 40){
+            //fake error
+            response.setStatus(HttpStatus.NOT_FOUND.value());
+            return "error";
+        }
+         else if (userData.getAge()>30){
+            //fake some security check failed
+            response.setStatus(HttpStatus.FORBIDDEN.value());
+            return "forbidden";
+        }
+        else {
+            //fake save
+            response.setStatus(HttpStatus.OK.value());
+            return "success";
+        }
+    }
 }
--- src/test/groovy/com/bkern/mvc/web/controller/JSONControllerTest.groovy
+++ src/test/groovy/com/bkern/mvc/web/controller/JSONControllerTest.groovy
@@ -4,6 +4,7 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import org.springframework.test.context.web.WebAppConfiguration
@@ -12,6 +13,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.web.context.WebApplicationContext
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -32,11 +34,44 @@ class JSONControllerTest {
     public void testGetData() throws Exception{
        this.mockMvc.perform(get("/getData/{id}","barry"))
         .andExpect(status().isOk())
-        .andExpect(content().contentType("application/json"))
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
         .andExpect(jsonPath('$.name').value("wayne"))
         .andExpect(jsonPath('$.age').value(99))
         .andExpect(jsonPath('$.sampleData').value('this is some sample data'))
         .andExpect(jsonPath('$.count').value(34))
 //        .andExpect(jsonPath('$.role').value(UserData.USER_ROLES.USER.value()))
+    }
+
+    @Test
+    void testSaveUser() throws Exception {
+        this.mockMvc.perform(
+                            post("/user/")
+                                    .accept(MediaType.APPLICATION_JSON)
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .content("{ \"age\": \"23\", \"name\": \"Bob\" }".getBytes()))
+                                .andExpect(status().isOk())
+                                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+    }
+
+    @Test
+    void testSaveUserNotFound() throws Exception {
+        this.mockMvc.perform(
+                post("/user/")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{ \"age\": \"43\", \"name\": \"Tim\" }".getBytes()))
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+    }
+
+    @Test
+    void testSaveUserSecurityError() throws Exception {
+        this.mockMvc.perform(
+                post("/user/")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{ \"age\": \"37\", \"name\": \"Bob\" }".getBytes()))
+                .andExpect(status().isForbidden())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
     }
 }
